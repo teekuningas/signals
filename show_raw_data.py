b18cfbd9d0bfe387e54b14614e711d201dc26cd3
Erkka Heinila <erkkahe@gmail.com>
2015-11-09 21:57:10 +0200

    Add script for simply plotting raw data from command line

---
+++ show_raw_data.py
@@ -0,0 +1,26 @@
+# Reads EGI or FIF and plots it 
+# Usage:
+#     python plot_raw_data.py /path/to/source/file
+import mne
+import sys
+
+
+def read_raw(filename):
+    if filename.endswith('.fif'):
+        raw = mne.io.Raw(filename, preload=True)
+    else:
+        # assume egi
+        raw = mne.io.read_raw_egi(filename)
+    return raw
+
+
+def main(filename):
+    raw = read_raw(filename)
+    raw.filter(l_freq=1, h_freq=30)
+    raw.plot()
+    wait_for_input = raw_input('Enter anything to quit.')
+
+
+if __name__ == '__main__':
+    cla = sys.argv
+    main(cla[1])
