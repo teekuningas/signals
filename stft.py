49192b3b7d82742301311df80b89a7001d927735
Erkka Heinila <erkkahe@gmail.com>
2016-01-30 15:03:07 +0200

    Allow stft plots to be paged

--- stft.py
+++ stft.py
@@ -8,9 +8,9 @@ raw = mne.io.Raw('/home/zairex/Code/cibr/data/graduaineisto/meditaatio/KH004_MED
 data = raw._data
 info = raw.info
 sfreq = info['sfreq']
-wsize = int(sfreq*4)
-tstep = int(wsize/2)
-freq_limit = 20
+wsize = int(sfreq*2)
+tstep = int(wsize/4)
+freq_limit = 100
 channels = [
     16, # middle front 
     75, # middle back
@@ -25,6 +25,8 @@ class STFTPlot():
         self.tfr = mne.time_frequency.stft(data, wsize, tstep)
         self.x = np.arange(0, self.tfr.shape[2]*tstep, tstep) / sfreq
         self.y = mne.time_frequency.stftfreq(wsize, sfreq)
+        self.window = 50
+        self.position = 0
 
         # find index for frequency limit
         self.freq_idx = int(self.y.shape[0] / ((sfreq / 2) / freq_limit))
@@ -32,11 +34,23 @@ class STFTPlot():
         fig, self.axarray = plt.subplots(2,2)
         key_release_cid = fig.canvas.mpl_connect('key_release_event', 
                                                  self.on_key_release)
-        self.plot_window(0, len(self.x))
+        self.plot_window(self.position, self.window)
+        # self.plot_window(0, len(self.x))
         plt.show()
 
     def on_key_release(self, event):
-        pass
+        if event.key == 'left':
+            if self.position - self.window >= 0:
+                self.position = self.position - self.window
+            else:
+                self.position = 0
+        if event.key == 'right':
+            if self.position + 2*self.window < len(self.x):
+                self.position = self.position + self.window
+            else:
+                self.position = len(self.x) - self.window - 1
+        
+        self.plot_window(self.position, self.position + self.window)
 
     def plot_window(self, start, end):
         for i in range(self.axarray.shape[0]):
