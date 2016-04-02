07b6ca3c6d41497cbeb43a88463eb01b5a8f4201
Erkka Heinila <erkkahe@gmail.com>
2016-04-02 17:13:58 +0300

    Implement whitening

--- fourier_ica.py
+++ fourier_ica.py
@@ -5,6 +5,10 @@
 import numpy as np
 import mne
 
+from mne.time_frequency.stft import stft
+
+from sklearn.decomposition import PCA
+
 
 class FourierICA(object):
 
@@ -27,11 +31,81 @@ class FourierICA(object):
         the number of channels supplied on decomposing data. Defaults to None.
     """
 
-    def __init__(self, n_components=None, max_pca_components=None):
-        pass
+    def __init__(self, wsize, sfreq, tstep=None, n_ica_components=None, n_pca_components=None, 
+                 zerotolerance=1e-14, lpass=1, hpass=80):
+        self.wsize = wsize
+        self.tstep = tstep
+        self.n_pca_components = n_pca_components
+        self.n_ica_components = n_ica_components
+        self.lpass = lpass
+        self.hpass = hpass
+        self.zerotolerance = zerotolerance
 
-    def fit(self, raw):
-        pass
+    def fit(self, data):
+        """ Fit data
+
+        Parameters
+        ---------
+        data : ndarray, shape (channels, times)
+
+        Returns
+        ------
+
+        """
+        # first do stft
+        stft_ = stft(data, self.wsize, self.tstep)
+        # remove outliers
+
+        # concatenate ft's to have two-dimensional data for ica
+        fts = [stft_[:, :, idx] for idx in range(stft_.shape[2])]
+        data2d = np.concatenate(fts, axis=1)
+
+        whitened = self._whiten(data2d)
+
+        import pdb; pdb.set_trace()
+
+        # do ica
+
+        # sort components
 
     def apply(self):
         pass
+
+    def _whiten(self, data):
+        """ Whiten data with PCA
+        """
+
+        # substract mean value from channels
+        mean_ = data.mean(axis=-1)
+        data -= mean_[:, np.newaxis]
+
+        # calculate eigenvectors and eigenvalues from covariance matrix
+        covmat = np.cov(data)
+        covmat = np.conj(covmat)
+        eigw, eigv = np.linalg.eigh(covmat)
+
+        # filter out components that are too small (or even negative)
+        valids = np.where(eigw > self.zerotolerance)[0]
+        eigw = eigw[valids]
+        eigv = eigv[:, valids]
+
+        # adjust number of copmonents
+        n_pca_components = self.n_pca_components
+        if not n_pca_components:
+            n_pca_components = len(valids)
+        elif n_pca_components > len(valids):
+            n_pca_components = len(valids)
+
+        # sort in descending order and take only n_pca_components of components
+        eigw = eigw[::-1][0:n_pca_components]
+        eigv = eigv[:, ::-1][:, 0:n_pca_components]
+
+        # construct whitening and dewhitening matrices
+        dsqrt = np.sqrt(eigw)
+        dsqrtinv = 1.0/dsqrt
+        self.whitening = np.dot(np.diag(dsqrtinv), eigv.T)
+        self.dewhitening = np.dot(eigv, np.diag(dsqrt))
+
+        # whiten the data
+        whitened = np.dot(self.whitening, data)
+        return whitened
--- fourier_ica_eeg.py
+++ fourier_ica_eeg.py
@@ -7,5 +7,5 @@ from fourier_ica import FourierICA
 if __name__ == '__main__':
     raw = mne.io.Raw('/home/zairex/Code/cibr/data/gradudemo/KH007_EOEC-pre.fif',
                      preload=True)
-    fica = FourierICA(n_components=5, max_pca_components=30)
-    fica.fit(raw)
+    fica = FourierICA(wsize=1000, sfreq=1000, n_ica_components=5, n_pca_components=30, lpass=1, hpass=40)
+    fica.fit(raw._data)
