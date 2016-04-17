408462aa616755d26bae41ea0c46d0e5a26f6f3b
Erkka Heinila <erkkahe@gmail.com>
2016-04-17 23:36:56 +0300

    Make small changes and add topomap plot for indepedent components

--- fourier_ica_eeg.py
+++ fourier_ica_eeg.py
@@ -12,11 +12,7 @@ def plot_whole_measurement(raw):
     """
     """
     wsize = 2000
-    fica = FourierICA(wsize=wsize, n_pca_components=20, 
-                      n_ica_components=15,
-                      sfreq=raw.info['sfreq'], hpass=4, lpass=15)
-    fica.fit(raw._data[:128, raw.first_samp:raw.last_samp])
-    freqs, source_stft = fica.source_stft
+    n_ica_components = 10
 
     try:
         triggers = mne.find_events(raw)[:, 0] - raw.first_samp
@@ -25,12 +21,25 @@ def plot_whole_measurement(raw):
     except:
         triggers = []
 
+    # drop non-data channels
+    raw.drop_channels(raw.info['ch_names'][128:])
+
+    fica = FourierICA(wsize=wsize, n_pca_components=20, 
+                      n_ica_components=n_ica_components,
+                      sfreq=raw.info['sfreq'], hpass=4, lpass=15)
+    fica.fit(raw._data[:, raw.first_samp:raw.last_samp])
+
+    source_stft = fica.source_stft
+    freqs = fica.freqs
+
     # ignore_times = range(480, 510)
     ignore_times = []
 
-    STFTPlot(freqs, source_stft, window_width=50, triggers=triggers,
-             ch_names=[str(i+1) for i in range(len(source_stft))],
-             ignore_times=ignore_times)
+    plot_ = STFTPlot(freqs, source_stft, window_width=50, triggers=triggers,
+                     ch_names=[str(i+1) for i in range(len(source_stft))],
+                     ignore_times=ignore_times)
+
+    plt.show()
 
 
 def _filter_triggers(triggers, sfreq, start, end, rad=10, overlap=5):
@@ -63,21 +72,25 @@ def _filter_triggers(triggers, sfreq, start, end, rad=10, overlap=5):
     return np.array(valid_triggers, dtype=triggers.dtype)
 
 
-def plot_epochs(raw):
-    wsize = 1000
+def plot_epochs(raw, layout):
+    wsize = 1000 # step size 500
     rad = 15
     sfreq = raw.info['sfreq']
+    n_ica_components = 5
 
     # find and filter triggers
     triggers = mne.find_events(raw)[:, 0]
     triggers = _filter_triggers(triggers, sfreq, raw.first_samp, raw.last_samp,
                                 rad=rad, overlap=rad/2)
 
+    # drop non-data channels
+    raw.drop_channels(raw.info['ch_names'][128:])
+
     # create epochs
     epochs = []
     for trigger in triggers:
         data = raw._data
-        epochs.append(data[:128, (trigger-rad*sfreq):(trigger+rad*sfreq)])
+        epochs.append(data[:, (trigger-rad*sfreq):(trigger+rad*sfreq)])
     
     # remove outliers or noisy data
 
@@ -86,13 +99,24 @@ def plot_epochs(raw):
     for epoch in epochs:
         data = epoch
         fica = FourierICA(wsize=wsize, n_pca_components=20, 
-                          n_ica_components=5, conveps=1e-13,
+                          n_ica_components=n_ica_components, conveps=1e-13,
                           sfreq=sfreq, hpass=8, lpass=14)
         fica.fit(data)
-        freqs, source_stft = fica.source_stft
+        freqs = fica.freqs
+        source_stft = fica.source_stft
 
-        STFTPlot(freqs, source_stft, window_width=60, window_height=5,
-                 ch_names=[str(i+1) for i in range(len(source_stft))])
+        fig_ = plt.figure()
+        for i in range(source_stft.shape[0]):
+            sensor_component = np.abs(fica.component_in_sensor_space(i))
+            tfr_ = mne.time_frequency.AverageTFR(raw.info, sensor_component, range(sensor_component.shape[2]), freqs, 1)
+            axes = fig_.add_subplot(source_stft.shape[0], 1, i + 1)
+            mne.viz.plot_tfr_topomap(tfr_, layout=layout, axes=axes, show=False)
+
+        plot_ = STFTPlot(freqs, source_stft, window_width=4*rad, 
+                         window_height=n_ica_components,
+                         ch_names=[str(i+1) for i in range(len(source_stft))])
+
+        plt.show()
 
 
 if __name__ == '__main__':
@@ -100,5 +124,10 @@ if __name__ == '__main__':
     raw = mne.io.Raw('/home/zairex/Code/cibr/data/gradudemo/KH005_MED-pre.fif',
                      preload=True)
 
+    layout_fname = 'gsn_129.lout'
+    layout_path = '/home/zairex/Code/cibr/materials/'
+    layout = mne.channels.read_layout(layout_fname, layout_path)
+
+
     # plot_whole_measurement(raw)
-    plot_epochs(raw)
+    plot_epochs(raw, layout)
