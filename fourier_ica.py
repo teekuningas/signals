c69f92be8bd6d6d7623af918d4dc13412e32c483
Erkka Heinila <erkkahe@gmail.com>
2016-05-05 15:01:05 +0300

    Add simple fourier ica example

---
+++ fourier_ica.py
@@ -0,0 +1,44 @@
+import mne
+import matplotlib.pyplot as plt
+import numpy as np
+
+from lib.fourier_ica import FourierICA
+from lib.stft import STFTPlot
+
+
+raw = mne.io.Raw('data/eoec-raw.fif', preload=True)
+
+layout_fname = 'gsn_129.lout'
+layout_path = 'data/'
+layout = mne.channels.read_layout(layout_fname, layout_path)
+
+wsize = 2000
+n_components = 5
+sfreq = raw.info['sfreq']
+
+# drop bad and non-data channels
+raw.drop_channels(raw.info['ch_names'][128:] + raw.info['bads'])
+
+# calculate fourier-ica
+fica = FourierICA(wsize=wsize, n_components=n_components,
+                  sfreq=raw.info['sfreq'], hpass=4, lpass=20)
+fica.fit(raw._data[:, raw.first_samp:raw.last_samp])
+
+source_stft = fica.source_stft
+freqs = fica.freqs
+fig_ = plt.figure()
+
+# plot components in sensor space on head topographies
+for i in range(source_stft.shape[0]):
+    sensor_component = np.abs(fica.component_in_sensor_space(i))
+    tfr_ = mne.time_frequency.AverageTFR(raw.info, sensor_component, 
+        range(sensor_component.shape[2]), freqs, 1)
+
+    axes = fig_.add_subplot(source_stft.shape[0], 1, i+1)
+    mne.viz.plot_tfr_topomap(tfr_, layout=layout, axes=axes, show=False)
+
+plot_ = STFTPlot(freqs, source_stft, window_width=50, 
+                 window_height=n_components,
+                 ch_names=[str(i+1) for i in range(len(source_stft))])
+
+plt.show()
