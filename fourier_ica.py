9f0c865b963f6c5c78510f049181b178f7638b17
Erkka Heinila <erkkahe@gmail.com>
2016-06-09 23:27:14 +0300

    Use AverageTFR for continuous fourier ica too

--- fourier_ica.py
+++ fourier_ica.py
@@ -12,7 +12,7 @@ layout_fname = 'gsn_129.lout'
 layout_path = 'data/'
 layout = mne.channels.read_layout(layout_fname, layout_path)
 
-wsize = 2000
+wsize = 2048
 n_components = 5
 sfreq = raw.info['sfreq']
 
@@ -45,10 +45,47 @@ for i in range(source_stft.shape[0]):
     axes = fig_.add_subplot(source_stft.shape[0], 1, i+1)
     axes.plot(x, y)
 
-# plot tfr's of source components
-plot_ = STFTPlot(freqs, source_stft, window_width=50, 
-                 window_height=n_components,
-                 ch_names=[str(i+1) for i in range(len(source_stft))])
+
+# mock info
+info = raw.info.copy()
+info['chs'] = info['chs'][0:5]
+
+fig_ = plt.figure()
+position = 0
+window = 30
+
+def update_tfr_plot(position):
+
+    fig_.clear()
+    for i in range(source_stft.shape[0]):
+        times = np.arange(window * position, window * (position + 1), 1.0)
+
+        # select subset of data
+        data = np.abs(source_stft[:, :, window*position:window*(position+1)])
+        
+        # pad if necessary
+        data = np.pad(data, ((0,0), (0,0), (0, window - data.shape[2])), 
+                      constant_values=0, mode='constant')
+
+        # use mne's AverageTFR for plotting
+	tfr_ = mne.time_frequency.AverageTFR(info, data, times, freqs, 1)
+	axes = fig_.add_subplot(source_stft.shape[0], 1, i + 1)
+	tfr_.plot(picks=[i], axes=axes, show=False, mode='logratio')
+
+    plt.draw()
+
+def on_key_release(event):
+    global position
+    if event.key == 'left' and position != 0:
+        position -= 1
+    elif event.key == 'right' and position < source_stft.shape[2] / window:
+        position += 1
+
+    update_tfr_plot(position)
+
+update_tfr_plot(position)
+
+cid = fig_.canvas.mpl_connect('key_release_event', on_key_release)
 
 # show everything
 plt.show()
