4995f43af805f4de42e53ae8ca3eea22e2d7a161
Erkka Heinila <erkkahe@gmail.com>
2016-09-21 19:18:25 +0300

    Update clean artifacts script

--- clean_artifacts.py
+++ clean_artifacts.py
@@ -11,40 +11,32 @@ def read_raw(filename):
     if filename.endswith('.fif'):
         raw = mne.io.Raw(filename, preload=True)
     else:
-        # assume egi
         raw = mne.io.read_raw_egi(filename, preload=True)
+        # assume egi
+        if filename.endswith('.001'):
+            stem = filename.split('.001')[0]
+            idx = 2
+            while True:
+                fname = stem + "." + '%03d' % idx
+                try:
+                    raw.append(mne.io.read_raw_egi(fname, preload=True))
+                except:
+                    break
+                idx += 1
     return raw
-
-
-def get_ica_path(filename):
-    last_part = filename.split('/')[-1]
-    without_extension = ''.join(last_part.split('.')[:-1])
-    with_new_extension = '.' + without_extension + '-ica.fif'
-    final = '/'.join(filename.split('/')[:-1] + [with_new_extension])
-    return final
-
 
 def main(from_file, to_file):
 
-    temp_ica = get_ica_path(to_file)
+    layout_fname = 'gsn_129.lout'
+    layout_path = '/home/zairex/Code/cibr/materials/'
+    layout = mne.channels.read_layout(layout_fname, layout_path)
+
     raw = read_raw(from_file)
 
-    raw.filter(l_freq=1, h_freq=30)
+    raw.filter(l_freq=1, h_freq=100)
 
-    raw.plot()
-
-    use_saved = raw_input('Use saved ica solution if found (y, n): ')
-
-    ica = None
-    if use_saved == 'y': 
-        try:
-            ica = mne.preprocessing.ica.read_ica(temp_ica)
-        except IOError:
-            print "Saved solution not found"
-    if not ica:
-        ica = mne.preprocessing.ICA(n_components=0.95, method='fastica')
-        ica.fit(raw)
-        ica.save(temp_ica)
+    ica = mne.preprocessing.ICA(n_components=0.95, method='fastica')
+    ica.fit(raw)
 
     sources = ica.get_sources(raw)
 
@@ -53,6 +45,7 @@ def main(from_file, to_file):
         for idx, amplitude in enumerate(source):
             source[idx] = amplitude / 5000.0
 
+    ica.plot_components(layout=layout, show=False)
     sources.plot()
 
     indices = raw_input('Please enter indices (starts from zero) of '
