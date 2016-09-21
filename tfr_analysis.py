62c04218b2a64c79bc7ea65c0117bc8089ec8b69
Erkka Heinila <erkkahe@gmail.com>
2016-09-21 19:30:17 +0300

    Update tfr_analysis.py script

--- tfr_analysis.py
+++ tfr_analysis.py
@@ -5,6 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
+from scipy.ndimage.filters import convolve
+
 from lib.load import cli_raws
 from lib.load import get_raw
 from lib.load import load_layout
@@ -15,19 +17,18 @@ from lib.utils import filter_triggers
 raws = cli_raws()
 
 if not raws:
-    raws = [get_raw('KH002', 'med')]
+    raws = [get_raw('KH003', 'eoec'), get_raw('KH003', 'med')]
+
+for raw in raws:
+    picks = mne.pick_types(raw.info, eeg=True, meg=True)
+    raw.drop_channels([name for idx, name in enumerate(raw.info['ch_names'])
+                       if idx not in picks])
 
 raw = raws[0]
 raw.append(raws[1:])
 
-# find triggers
-triggers = mne.find_events(raw)[:, 0]
-
-# drop unnecessary channels
-picks = mne.pick_types(raw.info, eeg=True, meg=True)
-raw.drop_channels([name for idx, name in enumerate(raw.info['ch_names'])
-		   if idx not in picks])
-
+# set triggers
+triggers = [95000, 205000]
 
 # get threshold parameter
 args = sys.argv
@@ -42,23 +43,14 @@ if '--channel' in args:
                     if arg == '--channel'][0])]
 else:
     # front, back, right, left
-    channels = [11, 75, 108, 45]
+    channels = [10, 71, 107, 44]
 
-wsize = 8192
-data = raw._data
+wsize = 32768
 
-stft = np.log(1 + np.abs(mne.time_frequency.stft(data, wsize)))
+stft = np.power(np.abs(mne.time_frequency.stft(raw._data, wsize)), 2)
 freqs = mne.time_frequency.stftfreq(wsize, raw.info['sfreq'])
 
-# standardize
-max_ = np.max(stft)
-min_ = np.min(stft)
-stft = (stft - min_) / (max_ - min_)
-
-# clip
-stft = np.clip(stft, 0, threshold)
-
-stft = stft - threshold/2
+stft = convolve(stft, np.ones((1, 51, 3))/(1*51*3))
 
 length_samples = stft.shape[2]
 length_s = (raw.last_samp - raw.first_samp) / raw.info['sfreq']
@@ -73,8 +65,8 @@ tfr = mne.time_frequency.AverageTFR(
 for channel in channels:
     fig_ = plt.figure()
     axes = fig_.add_subplot(1, 1, 1)
-    tfr.plot(picks=[channel], title=str(channel), fmin=1, fmax=100, 
-             show=False, axes=axes)
+    tfr.plot(picks=[channel], title=str(channel), fmin=1, fmax=40, 
+             show=False, axes=axes, mode='logratio', baseline=(0, 70))
 
     # plot triggers
     for trigger in triggers:
