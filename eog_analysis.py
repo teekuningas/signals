49c92e4408c5404e94f877cc90a1c29a6d9248e0
Erkka Heinila <erkkahe@gmail.com>
2016-11-17 02:18:41 +0200

    Update eog analysis script

--- eog_analysis.py
+++ eog_analysis.py
@@ -1,13 +1,32 @@
-import mne
 import sys
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+import mne
+
 fname = sys.argv[-1]
+
+PLOTS = 6
 
 raw = mne.io.Raw(fname, preload=True)
 
 picks = mne.pick_types(raw.info, meg=False, eog=True)
 
-eog_data = raw._data[picks]
+eog_data = raw._data[picks][0]
 
-import pdb; pdb.set_trace()
-print "kissa"
+events = mne.find_events(raw)
+
+event_data = np.zeros((len(eog_data),))
+for event in events:
+    if event[1] == 0:
+        event_data[event[0] - raw.first_samp] = np.min(eog_data) 
+
+for i in range(PLOTS):
+    plt.subplot(PLOTS, 1, i+1)
+    start = i*len(event_data) / PLOTS
+    end = (i+1)*len(event_data) / PLOTS - 1
+    plt.plot(np.arange(start, end)/raw.info['sfreq'], eog_data[start:end])
+    plt.plot(np.arange(start, end)/raw.info['sfreq'], event_data[start:end])
+
+plt.show()
